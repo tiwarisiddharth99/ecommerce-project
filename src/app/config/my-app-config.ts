09377c6f097fc7aa29b20b14353c731a5478d0aa
tiwarisiddharth99 <tiwarisiddharth288@gmail.com>
2023-03-26 15:33:34 +0530

    My first commit

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa8qmlscmCSD3uBV5d7',
+    issuer: 'https://dev-58238307.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
